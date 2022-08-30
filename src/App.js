805c6589c14a6dd8f11d9f8c43320dd1e804640f
Evan Beck <evandrydenbeck@gmail.com>
2022-08-29 23:05:04 -0400

    Login button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home';
+import Login from './components/Login';
 import Challenges from './components/Challenges';
 import Solutions from './components/Solutions';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Home/>
+      <Login/>
       <Challenges/>
       <Solutions/>
     </div>
---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Login() {
+
+    function githubLogin() {
+
+    }
+
+  return (
+    <div>
+        <button onClick={() => githubLogin}>Login with GitHub</button>
+    </div>
+  )
+}
+
+export default Login
